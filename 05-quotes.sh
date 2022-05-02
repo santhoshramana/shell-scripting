0e03041b414e2f2d1d4c380100ce5f2215ad4ad8
santhohsramana96 <patchigolladurgeshh@gmail.com>
2022-05-02 16:49:07 +0530

    date dummy ssd checll inputs quotes gup add

--- 05-quotes.sh
+++ 05-quotes.sh
@@ -4,6 +4,5 @@ echo 'apple mobile price is $100'
 echo "apple mobile price is \$100"
 
 TRAINING=DEVOPS
-
-echo 'training = $(TRAINING)'
-echo "training = \$(TRAINING)"
+echo 'training = ${TRAINING}'
+echo "training = \$\{TRAINING}"
