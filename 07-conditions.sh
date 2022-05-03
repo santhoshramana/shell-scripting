ab038c1ac7c5555d9955e36c1290c6b81cca5f25
santhohsramana96 <patchigolladurgeshh@gmail.com>
2022-05-03 12:30:17 +0530

    make file

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -6,13 +6,27 @@
 # If found in three forms
 
 # Simple if
-
 # if [ expression ]
 # then
 # commands
 # fi
 
-if [ 1 -eq 1 ]
-then
-  echo hi
+# Expressions are important
+# 1. String Tests
+# Operators : == , != , -z
+# 2. Number Tests
+# Operators : -eq , -ne , -le , -lt , -gt, -ge
+# 3. File Tests
+
+a="abc"
+if [ "$a" == "abc" ]; then
+  echo Both are equal
+fi
+
+if [ "$a" != "abc" ]; then
+  echo "Both are not equal"
+fi
+
+if [ -z $b ]; then
+  echo b variable is empty
 fi
