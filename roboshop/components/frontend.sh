689c6549e5364cdbc188dd047e9e85fea2fbff51
santhohsramana96 <patchigolladurgeshh@gmail.com>
2022-05-03 11:35:14 +0530

    make file

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+echo -e "\e[36m installing Nginx \e][0m"
 yum install nginx -y
 
+echo -e "\e[36m Downloading Nginx content \e][0m"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 
+echo -e "\e[36m Cleanup old Nginx \e][0m"
 rm -rf /usr/share/nginx/html/*
 cd /usr/share/nginx/html/
 unzip /tmp/frontend.zip
@@ -12,6 +15,6 @@ mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
-
+echo -e "\e[36m starting Nginx \e][0m"
 systemctl restart nginx
 systemctl enable nginx
