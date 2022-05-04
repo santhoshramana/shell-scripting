2cf4ae88cd24a93199d0e706c30a94dd04014bda
santhohsramana96 <patchigolladurgeshh@gmail.com>
2022-05-04 18:45:06 +0530

    s

---
+++ 08-functions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Variable : If we assign a name to set of data that is variable
+# Function : If we assign a name to set of commands that is function
+
+# func_name() {
+# commands
+# commands
+# }
+
+# func_name
+
+## Declare a function
+
+Print_Message() {
+  echo Hi,
+  echo Good Day
+  echo Welcome to DevOps Training
