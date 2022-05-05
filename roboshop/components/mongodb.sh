19e4b443febad0a99b6d6db6c9cffbb92c8df4b4
santhohsramana96 <patchigolladurgeshh@gmail.com>
2022-05-05 09:17:38 +0530

    s

---
+++ roboshop/components/mongodb.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+StatCheck() {
+ if [  $? -eq 0 ]; then
+   echo -e "\e[32mSUCCESS\e[0m"
+ else
+   echo -e "\e[31mFAILURE\e[0m"
+   exit 2
+ fi
+}
+
+Print() {
+  echo -e "\n ----------------- $1 -----------------" &>>LOG_FILE
+  echo -e "\e[36m $1 \e[0m"
+}
+
+USER_ID=$(id -u)
+if [  "$USER_ID" -ne 0 ]; then
+  echo you should be an root user
+  exit 1
+fi
+LOG_FILE=/tmp/roboshop.log
+rm -rf $LOG_FILE
+
+Print "Setup YUM Repos"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>LOG_FILE
+StatCheck $?
+
+Print "Install MongoDB"
+yum install -y mongodb-org &>>LOG_FILE
+Statcheck $?
+
+Print "Start MongoDB"
+systemctl enable mongod &>>LOG_FILE systemctl start mongod &>>LOG_FILE
+Statcheck $?
+
