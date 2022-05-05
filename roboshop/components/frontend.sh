6448cfe0f54c8e9319f75b05afd9351f35036f9e
santhohsramana96 <patchigolladurgeshh@gmail.com>
2022-05-05 07:53:28 +0530

    s

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -18,23 +18,25 @@ if [  "$USER_ID" -ne 0 ]; then
   echo you should be an root user
   exit 1
 fi
+LOG_FILE=/tmp/roboshop.log
+rm -rf $LOG_FILE
 
 Print "Installing Nginx"
-yum install nginx -y
+yum install nginx -y >>$LOG_FILE
 StatCheck $?
 
 Print "Downloading Nginx Content"
-curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >>$LOG_FILE
 StatCheck $?
 
 Print "Cleanup old Nginx"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* >>$LOG_FILE
 StatCheck $?
 
 cd /usr/share/nginx/html/
 
 Print "Extract Archive"
-unzip /tmp/frontend.zip $$ mv frontend-main/* . && mv static/* .
+unzip /tmp/frontend.zip >>$LOG_FILE $$ mv frontend-main/* . >>$LOG_FILE && mv static/* . >>$LOG_FILE
 StatCheck $?
 
 
@@ -43,7 +45,7 @@ mv localhost.conf /etc/nginx/default.d/roboshop.conf
 StatusCheck $?
 
 Print "starting Nginx"
-systemctl restart nginx && systemctl && enable nginx
+systemctl restart nginx >>$LOG_FILE && systemctl enable nginx >>$LOG_FILE
 StatusCheck $?
 
 
