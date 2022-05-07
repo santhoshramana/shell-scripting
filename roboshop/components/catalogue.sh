9e09e390d1ea9c7253ddb7ba9576a6dee3cb77ff
santhohsramana96 <patchigolladurgeshh@gmail.com>
2022-05-07 13:11:52 +0530

    dsfgdfsg

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -33,3 +33,7 @@ StatCheck $?
 Print "Installing APP Dependencies"
 cd /home/${APP_USER}/catalogue &>>${LOG_FILE} && npm install &>>${LOG_FILE}
 StatCheck $?
+
+Print "App User Permission"
+chown -R ${APP_USER}:${APP_USER} /home/${APP_USER}
+StatCheck $?
