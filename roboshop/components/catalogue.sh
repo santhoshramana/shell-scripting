f69b459638b59b0eb982cec467d36dcc066454c3
santhohsramana96 <patchigolladurgeshh@gmail.com>
2022-05-07 12:37:37 +0530

    s

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -15,9 +15,8 @@ Print "Add Application User"
 id ${APP_USER} &>>${LOG_FILE}
 if [ $? -ne 0 ]; then
   useradd ${APP_USER} &>>${LOG_FILE}
-  StatCheck $?
 fi
-
+StatCheck $?
 Print "App content Download"
 curl -f -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
 StatCheck $?
