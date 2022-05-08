6822c09825b636885b15c2149e43a2aeeab5ae53
santhohsramana96 <patchigolladurgeshh@gmail.com>
2022-05-08 18:09:08 +0530

    dsfgdfsg

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -61,11 +61,7 @@ NODEJS() {
 
 
   Print "Setup SystemD File"
-  sed -i  -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' \
-          -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' \
-          -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' \
-          -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' \
-          /home/roboshop/${COMPONENT}/systemd.service &>>${LOG_FILE} && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service  &>>${LOG_FILE}
+  sed -i  -e 's/MONGO_DNSNAME/mongodb.roboshopinternal/' -e 's/REDIS_ENDPOINT/redis.roboshopinternal/' -e 's/MONGO_ENDPOINT/mongodb.roboshopinternal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshopinternal/' /home/roboshop/${COMPONENT}/systemd.service &>>${LOG_FILE} && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service  &>>${LOG_FILE}
   StatCheck $?
 
 
