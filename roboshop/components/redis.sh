396a0d5d4a1d9bf9c1a58d168512790615e951cf
santhohsramana96 <patchigolladurgeshh@gmail.com>
2022-05-08 12:55:14 +0530

    dsfgdfsg

---
+++ roboshop/components/redis.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source components/common.sh
+
+
+Print "Yum Repos"
+url -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>${LOG_FILE}
+StatCheck $?
+
+Print "Redis Install"
+yum intall redis -y &>>${LOG_FILE}
+StatCheck $?
+
+Print "Updated redis config"
+if [ -f /etc/redis.conf]; then
+ sed -i -e 's/127/0.0.1/0.0.0.0/' /etc/redis.conf
+fi
+if [ -f /etc/redis/redis.conf]; then
+ sed -i -e 's/127/0.0.1/0.0.0.0/' /etc/redis/redis.conf
+fi
+StatCheck $?
+
+Print "Redis service starting"
+systemctl enable redis &>>${LOG_FILE} && systemctl start redis &>>${LOG_FILE}
+StatCheck $?
