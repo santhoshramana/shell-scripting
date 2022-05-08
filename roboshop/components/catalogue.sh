e19c562ff1e07dc7272b44e945e3c0fb353fabb4
santhohsramana96 <patchigolladurgeshh@gmail.com>
2022-05-08 10:16:23 +0530

    dsfgdfsg

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -37,7 +37,7 @@ chown -R ${APP_USER}:${APP_USER} /home/${APP_USER}
 StatCheck $?
 
 Print "SystemD File"
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshopinternal/' /home/roboshop/catalogue/systemd.service &>>${LOG_FILE} && mv home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>${LOG_FILE}
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshopinternal/' /home/roboshop/catalogue/systemd.service &>>${LOG_FILE} && mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>${LOG_FILE}
 StatCheck $?
 
 Print "Catalog Service"
