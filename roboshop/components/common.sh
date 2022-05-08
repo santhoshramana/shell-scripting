15c51092f3cd8fa5db3cbcb581a4cc75383e41c2
santhohsramana96 <patchigolladurgeshh@gmail.com>
2022-05-08 13:25:24 +0530

    dsfgdfsg

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -21,3 +21,50 @@ LOG_FILE=/tmp/roboshop.log
 rm -rf $LOG_FILE
 
 APP_USER=roboshop
+
+NODEJS() {
+
+  Print "Configure yum repos"
+  curl -f -s -L https://rpm.nodesource.com/setup_lts.x | bash - &>>${LOG_FILE}
+  StatCheck $?
+
+  Print "Install NodeJS"
+  yum install nodejs gcc-c++ -y &>>${LOG_FILE}
+  StatCheck $?
+
+  id ${APP_USER} &>>${LOG_FILE}
+  if [ $? -ne 0 ]; then
+    Print "Add Application User"
+    useradd ${APP_USER} &>>${LOG_FILE}
+    StatCheck $?
+  fi
+
+  Print "App content Download"
+  curl -f -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>${LOG_FILE}
+  StatCheck $?
+
+  Print "Clear old Content"
+  rm -rf /home/${APP_USER}/${COMPONENT} &>>${LOG_FILE}
+  StatCheck $?
+
+  Print "Extracting Content"
+  cd /home/${APP_USER} &>>${LOG_FILE} && unzip -o /tmp/${COMPONENT}.zip &>>${LOG_FILE} && mv ${COMPONENT}-main ${COMPONENT} &>>${LOG_FILE}
+  StatCheck $?
+
+  Print "Installing APP Dependencies"
+  cd /home/${APP_USER}/${COMPONENT} &>>${LOG_FILE} && npm install &>>${LOG_FILE}
+  StatCheck $?
+
+  Print "App User Permission"
+  chown -R ${APP_USER}:${APP_USER} /home/${APP_USER}
+  StatCheck $?
+
+  Print "SystemD File"
+  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshopinternal/' /home/roboshop/${COMPONENT}/systemd.service &>>${LOG_FILE} && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/catalog${COMPONENT}ue.service &>>${LOG_FILE}
+  StatCheck $?
+
+  Print "Catalog Service"
+  systemctl daemon-reload &>>${LOG_FILE} && systemctl restart ${COMPONENT} &>>${LOG_FILE} && systemctl enable ${COMPONENT} &>>${LOG_FILE}
+  StatCheck $?
+
+}
