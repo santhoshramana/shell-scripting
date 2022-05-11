02320a8d43903d3fa08024a07dce4c53c6a04b80
santhohsramana96 <patchigolladurgeshh@gmail.com>
2022-05-11 16:31:48 +0530

    dsfgdfsg

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -3,16 +3,16 @@
 source components/common.sh
 
 Print "Configure YUM repos"
-curl -f -s -L -o curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>LOG_FILE
+curl -f -s -L -o curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>${LOG_FILE}
 StatCheck $?
 
 
 Print "MySQL"
-yum install mysql-community-server -y &>>LOG_FILE
+yum install mysql-community-server -y &>>${LOG_FILE}
 StatCheck $?
 
 Print "Start Mysql"
-systemctl enable mysql &>>LOG_FILE && systemctl start mysql &>>LOG_FILE
+systemctl enable mysql &>>${LOG_FILE} && systemctl start mysql &>>${LOG_FILE}
 StatCheck $?
 
 
