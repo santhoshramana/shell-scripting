c2c1f25eb91d0ace847901c568aea64a3fd02f51
santhohsramana96 <patchigolladurgeshh@gmail.com>
2022-05-17 20:00:19 +0530

    -

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId'| sed -e 's/"//g')
+if [ -z "$1" ]; then
+  echo -e "\e[31mInput Machine Name is needed\e[0m"
+  exit 1
+fi
 
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId'| sed -e 's/"//g')
+SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=allow-all-from-public | jq '.SecurityGroups[].Groupid' | sed -e 's/"//g')
 echo $AMI_ID
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --tag-specification "ResourceType=instance,Tags=[{key=Name,Value=${COMPONENT}}]" | jq
