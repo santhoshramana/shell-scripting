f0c37fe070d77c31abff6b14dc02b3a2e5a2ac35
santhohsramana96 <patchigolladurgeshh@gmail.com>
2022-05-18 11:37:39 +0530

    -asdfssdfdfveadsfsdf

--- create-ec2.sh
+++ create-ec2.sh
@@ -11,11 +11,13 @@ ZONE_ID="Z083275214GG1KG246KTC"
 AMI_ID="ami-0bb6af715826253bf"
 #AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=allow-all-from-public | jq  '.SecurityGroups[].GroupId' | sed -e 's/"//g')
+
 echo $AMI_ID
 PRIVATE_IP=$(aws ec2 run-instances \
     --image-id "${AMI_ID}" \
     --instance-type t3.micro \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
+    --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
     --security-group-ids "${SGID}" \
     | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
