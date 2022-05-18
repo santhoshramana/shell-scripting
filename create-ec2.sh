8c03fd68f794a006497105233c5873b5578e26c8
santhohsramana96 <patchigolladurgeshh@gmail.com>
2022-05-18 08:44:03 +0530

    -

--- create-ec2.sh
+++ create-ec2.sh
@@ -16,7 +16,6 @@ aws ec2 run-instances \
     --image-id ${AMI_ID} \
     --instance-type t2.micro \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
-    --instance-market-options "MarketType=spot",SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
     --security-group-ids ${SGID} \
     | jq
 
